1848c4c58e7b2a2c923006c92b94d7e151eab0b3
Brian Mego <brianmego@gmail.com>
2022-12-01 21:55:19 -0600

    day1

---
+++ day1/src/main.rs
@@ -0,0 +1,19 @@
+use std::fs;
+
+fn main() {
+    let inp_str = fs::read_to_string("data/inp2.txt").expect("Unable to load file");
+    let mut totals = vec![];
+    for group in inp_str.trim().split("\n\n") {
+        let mut total = 0;
+        for snack in group.split('\n') {
+            total += snack.parse::<i32>().unwrap();
+        }
+        totals.push(total);
+    }
+    totals.sort();
+
+    let largest = totals[totals.len() - 1];
+    let largest_three: i32 = totals[totals.len() - 3..].iter().sum();
+    println!("Largest: {largest}");
+    println!("Largest three: {largest_three}");
+}
