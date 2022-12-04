9d80230e95c6d1c5e80a5359626c36a7e1eafb1e
Brian Mego <brianmego@gmail.com>
2022-12-04 09:45:38 -0600

    Cleaner iteration

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -13,7 +13,7 @@ fn main() {
     totals.sort();
 
     let largest = totals[totals.len() - 1];
-    let largest_three: i32 = totals[totals.len() - 3..].iter().sum();
+    let largest_three: i32 = totals.into_iter().rev().take(3).sum();
     println!("Largest: {largest}");
     println!("Largest three: {largest_three}");
 }
